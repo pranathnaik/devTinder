2a11ec72968cf4a8ccfcf74c0252519069d858bd
Pranath Naik <pranathnaik12@gmail.com>
2025-05-24 00:10:30 +0530

    Remove database.js from Git tracking

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://pranathnaik12:u9W6azcvo9X3fW3L@learnmongodb.vrewz.mongodb.net/?retryWrites=true&w=majority&appName=learnmongodb"
-  );
-};
-
-module.exports = connectDB;
